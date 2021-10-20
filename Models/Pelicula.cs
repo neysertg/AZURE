8b258f5e060304f637acf6deccb7a089249ea788
Diegoo25 <diego_quispe14@usmp.pe>
2021-10-19 22:51:31 -0500

    .

---
+++ Models/Pelicula.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZURE_FINAL.Models
+{
+    public class Pelicula
+    {
+        [JsonProperty(PropertyName = "id")]
+          [Display(Name="ID")]
+        public String Id { get; set; }
+         [Display(Name="NOMBRE")]
+
+        public String Nombre { get; set; }
+         [Display(Name="APE_PATERNO")]
+        public String Ape_paterno { get; set; }
+         [Display(Name="APE_MATERNO")]
+        public String Ape_materno { get; set; }
+         [Display(Name="CURSO")]
+        public String Curso { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
